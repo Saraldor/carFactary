fa77a8d15e7fc4b07551655e8e39d88fce814700
christer Holm <ch@star78.se>
2021-09-16 10:11:43 +0200

    Test of Car registration number (Refactoring)

--- src/main/java/CarFactory.java
+++ src/main/java/CarFactory.java
@@ -1,10 +1,22 @@
 public class CarFactory {
     private String registrationNumber;
-
+    private String color;
 
     public CarFactory() {
         setRegistrationNumber(randomRegistration());
+        color = "Black";
+    }
+    public CarFactory(String color) {
+        setRegistrationNumber(randomRegistration());
+        this.color = color;
+    }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public String createCarModel() {
@@ -20,21 +32,19 @@ public class CarFactory {
         this.registrationNumber = registrationNumber;
     }
 
-    static String randomRegistration()
+    public String randomRegistration()
     {
-        // Generate three random uppercase letters
-        int letter1 = 65 + (int)(Math.random() * (90 - 65));
-        int letter2 = 65 + (int)(Math.random() * (90 - 65));
-        int letter3 = 65 + (int)(Math.random() * (90 - 65));
-
-        // Generate four random digits
-        int number1 = (int)(Math.random() * 10);
-        int number2 = (int)(Math.random() * 10);
-        int number3 = (int)(Math.random() * 10);
-
-        // Display number plate
-        return ("" + (char)(letter1) + ((char)(letter2)) +
-                ((char)(letter3)) + number1 + number2 + number3);
+        return ("" + (char) (getLetter()) + ((char) (getLetter())) +
+                ((char) (getLetter())) + "" + getNumber() + getNumber() + getNumber());
     }
+
+    public int getNumber() {
+        return (int) (Math.random() * 10);
+    }
+
+    public int getLetter() {
+        return 65 + (int) (Math.random() * (90 - 65));
+    }
+
 
 }
--- src/test/java/CarFactoryTest.java
+++ src/test/java/CarFactoryTest.java
@@ -7,12 +7,22 @@ public class CarFactoryTest {
     private CarFactory carFactory;
 
     @Test
-    public void new_car_with_color(){
+    public void new_car_with_registration_number(){
         carFactory = new CarFactory();
 
         assertEquals( 6, carFactory.getRegistrationNumber().length());
         assertFalse( (carFactory.getRegistrationNumber().length() < 6), "This will fail" );
     }
+    @Test
+    public void new_car_with_parameter_color(){
+        carFactory = new CarFactory("Blue");
 
+        assertEquals( "Blue", carFactory.getColor());
+        assertFalse( (carFactory.getColor().length() == 0), "This will fail" );
+
+        carFactory = new CarFactory();
+        assertEquals( "Black", carFactory.getColor());
+    }
 
 }
+
